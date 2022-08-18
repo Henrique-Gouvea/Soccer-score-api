b1f8c5357a9306c55632ad3855cace6cc8150182
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-08-18 16:16:59 -0300

    feat(routes): Criando rota para login (users)

---
+++ app/backend/src/routes/loginRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import UserController from '../controllers/usersController';
+import { UserService } from '../services/usersService';
+
+const userService = new UserService();
+const userController = new UserController(userService);
+
+const loginRouter = Router();
+
+loginRouter.post('/', (req, res) => userController.create(req, res));
+
+export default loginRouter;
