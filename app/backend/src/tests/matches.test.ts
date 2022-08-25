b66b996bb0de2cd5361f69b04ce822ed8b842003
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-08-25 16:58:25 -0300

    feat(test):

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -8,39 +8,61 @@ import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
 import { IMatches } from '../interfaces/Matches/Matches';
-import {mockMatches} from './mocks/match'
+import {mockMatches, mockCreateMatches} from './mocks/match'
 import Matches from '../database/models/matches';
+import { IToken } from '../interfaces/Providers/IToken';
+import TokenProvider from '../providers/tokenProvider';
+
+
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Teste para rota matches', () => {
+  describe('List', () => {
+    beforeEach(() => {
+      sinon.stub(Matches,'findAll').resolves([mockMatches as unknown as Matches]);
+    })
 
-  beforeEach(() => {
-    sinon.stub(Matches,'findAll').resolves([mockMatches as unknown as Matches]);
-  })
+    afterEach(() => {
+      sinon.restore();
+    })
 
-  afterEach(() => {
-    sinon.restore();
-  })
+    it('retorno status = 200', async () => {
+      const response  = await chai.request(app).get('/matches')
+      expect(response.status).to.equal(200);
+    });
 
-  it('retorno status = 200', async () => {
-    const response  = await chai.request(app).get('/matches')
-    expect(response.status).to.equal(200);
+    it('retorno body matches', async () => {
+      const response  = await chai.request(app).get('/matches')
+      const [match] = response.body;
+      expect(match.id).to.equal(mockMatches.id)
+      expect(match.homeTeam).to.equal(mockMatches.homeTeam)
+      expect(match.homeTeamGoals).to.equal(mockMatches.homeTeamGoals)
+      expect(match.awayTeam).to.equal(mockMatches.awayTeam)
+      expect(match.awayTeamGoals).to.equal(mockMatches.awayTeamGoals)
+      expect(match.inProgress).to.equal(mockMatches.inProgress)
+    });
   });
 
-//   it('retorno para filtro partida em progresso', async () => {
-//     const response = await chai.request(app).get('/matches');
-//     const match = response.body[0] as IMatches;
-//     expect(match).to.be.deep.equal(mock);
-//     expect(response.status).to.equal(200);
-//   });
+  describe('Create', () => {
+    beforeEach(() => {
+      sinon.stub(Matches,'create').resolves(mockCreateMatches as unknown as Matches);
+    })
 
-//   it('retorno para filtro partida finalizada', async () => {
-//     const response = await chai.request(app).get('/matches');
-//     const match = response.body[0] as IMatches;
-//     expect(match).to.be.deep.equal(mock[0]);
-//     expect(response.status).to.equal(200);
-//   });
+    afterEach(() => {
+      sinon.restore();
+    })
+
+    it('retorno status = 401', async () => {
+      const response  = await chai.request(app).post('/matches')
+      expect(response.status).to.equal(401);
+    });
+
+    it('Retorno mensagem invalid token', async () => {
+      const response  = await chai.request(app).post('/matches')
+      expect(response.body.message).to.equal('Invalid token');
+    });
+  });
 });
--- app/backend/src/tests/mocks/match.ts
+++ app/backend/src/tests/mocks/match.ts
@@ -1,17 +1,24 @@
 import Matches from '../../database/models/matches';
-import { IMatches } from '../../interfaces/Matches/Matches';
+import { CreateMatches, IMatches } from '../../interfaces/Matches/Matches';
 
-export const mockMatches: IMatches =  {
-    id: 42,
-    homeTeam: 6,
-    homeTeamGoals: 1,
-    awayTeam: 1,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: 'Ferroviária'
-    },
-    teamAway: {
-      teamName: 'Avaí/Kindermann'
-    }
+export const mockMatches: IMatches = {
+  id: 42,
+  homeTeam: 6,
+  homeTeamGoals: 1,
+  awayTeam: 1,
+  awayTeamGoals: 0,
+  inProgress: true,
+  teamHome: {
+    teamName: 'Ferroviária'
+  },
+  teamAway: {
+    teamName: 'Avaí/Kindermann'
   }
+}
+
+export const mockCreateMatches: CreateMatches = {
+  homeTeam: 16, // O valor deve ser o id do time
+  awayTeam: 8, // O valor deve ser o id do time
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+}
