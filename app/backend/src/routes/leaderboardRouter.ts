3ff95d3197489820c0c2ebbdb18b3ad2f26872f2
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-08-25 20:27:02 -0300

    feat(routes): Criando rota home para learderboard

---
+++ app/backend/src/routes/leaderboardRouter.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import LearderboardController from '../controllers/learderboardController';
+import LearderboardService from '../services/learderboardService';
+// import TokenProvider from '../providers/tokenProvider';
+// import CryptoProvider from '../providers/cryptoProvider';
+// import TokenMiddleware from '../middleware/tokenMiddleware';
+
+// const tokenProv = new TokenProvider();
+// const cryptoProv = new CryptoProvider();
+
+const learderboardService = new LearderboardService();
+const learderboard = new LearderboardController(learderboardService);
+// const tokenMiddleware = new TokenMiddleware(tokenProv);
+
+const leaderboardRouter = Router();
+
+leaderboardRouter.get('/home', (req, res, next) => learderboard.login(req, res, next));
+
+// leaderboardRouter.use((req, res, next) => tokenMiddleware.checkTokenMiddleware(req, res, next));
+
+export default leaderboardRouter;
